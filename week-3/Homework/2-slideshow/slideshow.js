ca199112e34f89b1f6cb07db0c3587f24b805932
Elir25 <elirendiles@gmail.com>
2021-04-06 11:46:55 +0200

    changing the css in the slideshow homework

--- week-3/Homework/2-slideshow/slideshow.js
+++ week-3/Homework/2-slideshow/slideshow.js
@@ -1,17 +1,18 @@
 // Write your code here
 let i = 0;
 let images = [];
-let time = 3000;
+let time = 2000;
 //images 
 images[0] = "images/anastasia-ulyanova-aFqfi1ASUJc-unsplash.jpg";
 images[1] = "images/jamie-street-uNNCs5kL70Q-unsplash.jpg";
 images[2] = "images/karsten-winegeart--_gMskl-uoc-unsplash.jpg";
 images[3] = "images/tran-mau-tri-tam--81lVsfM4gQ-unsplash.jpg";
 
-
+//butons
 let backBtn = document.querySelector('#back');
 let forwardBtn = document.querySelector('#forward');
 let autoBtn = document.querySelector('#auto');
+
 //change the images manually (forward)
 forwardBtn.addEventListener('click', () => {
   document.slide.src = images[i];
@@ -21,8 +22,12 @@ forwardBtn.addEventListener('click', () => {
 });
 //change the images backwards (back)
 backBtn.addEventListener('click', () => {
+  /** for (var i = arr.length - 1; i >= 0; i--) {
+	console.log(arr[i]);
+}*/
+  document.slide.src = images[i];
   
-})
+});
 
 //change the images every 3 seconds (auto)
 autoBtn.addEventListener('click', changeImg)
